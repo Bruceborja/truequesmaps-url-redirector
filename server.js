fbb80e04cd032570f9b87c6577fbce3aecd72e38
Bruceborja <bruceborja2@gmail.com>
2025-08-19 00:10:47 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
-app.use(cors({
-    origin: '*',
-    methods: ['GET'],
-    allowedHeaders: ['Content-Type'],
-}));
-
 app.get('/get-ngrok-url', (req, res) => {
-    const ngrokUrl = process.env.CURRENT_NGROK_URL;
+    let ngrokUrl = process.env.CURRENT_NGROK_URL;
+
+    // Añadir https:// si no está presente
+    if (ngrokUrl && !ngrokUrl.startsWith('https://') && !ngrokUrl.startsWith('http://')) {
+        ngrokUrl = 'https://' + ngrokUrl;
+    }
 
     if (ngrokUrl) {
         res.type('text/plain');
@@ -17,9 +12,4 @@ app.get('/get-ngrok-url', (req, res) => {
     } else {
         res.status(500).send('Ngrok URL not configured.');
     }
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-    console.log(`Redirector app listening on port ${PORT}`);
 });
